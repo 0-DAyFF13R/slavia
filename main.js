8fc07ef3ce9f8a1e5f8b69c28ed4fcd2a8a762bc
Daffier <aleks.anashin@gmail.com>
2023-06-03 18:44:41 +0300

    Footer add, header fix

---
+++ main.js
@@ -0,0 +1,29 @@
+let intro = $("#intro");
+let header = $("#header");
+let introH = intro.innerHeight();
+let headerH = header.innerHeight();
+let scrollTop = $(window).scrollTop();
+
+
+
+/* Header class on scroll
+=====================================*/
+
+headerScroll();
+
+$(window).on("scroll  resize", function () {
+    headerScroll();
+});
+
+function headerScroll() {
+    introH = intro.innerHeight();
+    headerH = header.innerHeight();
+
+    let scrollTop = $(this).scrollTop();
+
+    if (scrollTop >= (introH - headerH)) {
+        header.addClass("header_dark");
+    } else {
+        header.removeClass("header_dark");
+    }
+}
